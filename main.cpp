a5cf78b1b26f5a68765051fd511018a9fd977d77
Guilherme Delgado Martins <guilhermedelgado876@gmail.com>
2024-04-08 22:29:39 -0300

    refactor: organizando e pensando sobre o mapa

--- main.cpp
+++ main.cpp
@@ -12,14 +12,6 @@ ALLEGRO_EVENT ev;
 struct RETANGULO {
     float x1,y1,w,h,traco;
     ALLEGRO_COLOR cor;
-
-    void desenhaRetangulo(RETANGULO r) {
-    if (r.traco == 0){
-        al_draw_filled_rectangle(r.x1,r.y1,r.x1+r.w,r.y1+r.h,r.cor);
-    }else{
-        al_draw_rectangle(r.x1,r.y1,r.x1+r.w,r.y1+r.h,r.cor,r.traco);
-    }
-}
 };
 
 
@@ -43,13 +35,13 @@ int main(){
 
     al_clear_to_color(al_map_rgb(255,255,255));
 
-    RETANGULO r = {600,80,15,15, 10, al_map_rgb(0,255,0)};
+    RETANGULO r = {600,80,15,15, 0,al_map_rgb(0,255,0)};
 
     for(y = 0; y < 600; y += 20){
         for(x = 0; x < 800; x+= 20){
-         desenhaRetangulo(r);
+            desenhaRetangulo(r);
 
-         r.x1 = x;
+            r.x1 = x;
         }
         r.y1 = y;
     }
