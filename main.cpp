d1ebf8a618df79d941d54c85d194c954cf8c1191
diego <136401880+DKB-17@users.noreply.github.com>
2024-04-15 19:01:24 -0300

    feat: mudanca de logica, intruduzindo POO

--- main.cpp
+++ main.cpp
@@ -12,21 +12,31 @@ ALLEGRO_DISPLAY *janela = NULL;
 ALLEGRO_EVENT_QUEUE *fEventos = NULL;
 ALLEGRO_EVENT ev;
 
-struct RETANGULO {
-    float x1,y1,w,h,traco;
-    ALLEGRO_COLOR cor;
+int cellSize = 30;
+int cellCount = 25;
+
+class Comida {
+public:
+    int positionX = 5;
+    int positionY = 6;
+    ALLEGRO_COLOR cor = al_map_rgb(43, 51,24);
+
+    void Desenhar(){
+        al_draw_filled_rectangle(positionX * cellSize, positionY * cellSize, (positionX * cellSize) + cellSize, (positionY * cellSize) + cellSize, cor);
+    }
 };
 
 
-void desenhaRetangulo(RETANGULO r) {
+/*void desenhaRetangulo(RETANGULO r) {
     if (r.traco == 0){
         al_draw_filled_rectangle(r.x1,r.y1,r.x1+r.w,r.y1+r.h,r.cor);
     }else{
         al_draw_rectangle(r.x1,r.y1,r.x1+r.w,r.y1+r.h,r.cor,r.traco);
     }
 }
+*/
 
-bool colideRetangulo(RETANGULO r1, RETANGULO r2){
+/*bool colideRetangulo(RETANGULO r1, RETANGULO r2){
     if ( r2.x1+r2.w > r1.x1 &&
          r1.x1+r1.w > r2.x1 &&
          r2.y1+r2.h > r1.y1 &&
@@ -36,6 +46,7 @@ bool colideRetangulo(RETANGULO r1, RETANGULO r2){
         return false;
     }
 }
+*/
 
 
 int main(){
@@ -45,39 +56,36 @@ int main(){
     al_install_keyboard();
     al_init_image_addon();
 
-    int x=800;
-    int y=600;
-
-    janela = al_create_display(x, y);
+    janela = al_create_display(cellSize * cellCount, cellSize * cellCount);
     al_set_window_position(janela, 200,200);
     al_set_window_title(janela, "RETRO SNAKE");
 
-    ALLEGRO_TIMER *timer = al_create_timer(1.0 / 30.0);
+    float time = (1.0 / 60.0);
+    ALLEGRO_TIMER *timer = al_create_timer(time);
 
     fEventos = al_create_event_queue();
     al_register_event_source(fEventos, al_get_display_event_source(janela));
     al_register_event_source(fEventos, al_get_timer_event_source(timer));
     al_register_event_source(fEventos, al_get_keyboard_event_source());
 
-    ALLEGRO_BITMAP *maca = al_load_bitmap("./png/apple.png");
-    ALLEGRO_BITMAP *bg = al_load_bitmap("./png/background.png");
+    //ALLEGRO_BITMAP *maca = al_load_bitmap("./png/apple.png");
+    //ALLEGRO_BITMAP *bg = al_load_bitmap("./png/background.png");
 
 
-    ALLEGRO_COLOR cor_grama = al_map_rgb(1,200,0);
-    ALLEGRO_COLOR cor_cobra = al_map_rgb(241, 94,255);
+    ALLEGRO_COLOR bg = al_map_rgb(173,204,96);
+    ALLEGRO_COLOR comida = al_map_rgb(43, 51,24);
+    ALLEGRO_COLOR cobra = al_map_rgb(43, 51,24);
 
-    int xm = rand() % x;
-    int ym = rand() % y;
 
-    RETANGULO cCobra = {float(x/2),float(y/2),30,30,0,cor_cobra};
+    //RETANGULO cCobra = {float(x/2),float(y/2),30,30,0,cor_cobra};
 
+    Comida comi = Comida();
     while(true){
 
-        al_wait_for_event_timed(fEventos, &ev, 0.3);
-        al_clear_to_color(cor_grama);
-        al_draw_bitmap(bg, 0, 0, 0);
-        al_draw_bitmap(maca, xm, ym, 0);
-        desenhaRetangulo(cCobra);
+        al_wait_for_event_timed(fEventos, &ev, 0.06);
+        al_clear_to_color(bg);
+        comi.Desenhar();
+        /*desenhaRetangulo(cCobra);
         al_flip_display();
 
         if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
@@ -98,9 +106,11 @@ int main(){
         if (cCobra.x1 < 0 || cCobra.x1 > x || cCobra.y1 < 0 || cCobra.y1> y){
             break;
         }
+        */
     }
-    al_destroy_bitmap(maca);
-    al_destroy_bitmap(bg);
+
+    //al_destroy_bitmap(maca);
+    //al_destroy_bitmap(bg);
     al_destroy_display(janela);
     al_destroy_event_queue(fEventos);
 }
