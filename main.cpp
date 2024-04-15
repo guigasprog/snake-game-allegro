971d775facdd1c82db1c2bfb67377d9cdecacbf0
diego <136401880+DKB-17@users.noreply.github.com>
2024-04-14 23:12:20 -0300

    feat: criacao de tabuleiro e objetos cobra e maca

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <allegro.h>
 #include <allegro_primitives.h>
 #include <conio.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -23,34 +24,99 @@ void desenhaRetangulo(RETANGULO r) {
     }
 }
 
+bool colideRetangulo(RETANGULO r1, RETANGULO r2){
+    if ( r2.x1+r2.w > r1.x1 &&
+         r1.x1+r1.w > r2.x1 &&
+         r2.y1+r2.h > r1.y1 &&
+         r1.y1+r1.h > r2.y1   ){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 int main(){
+
     al_init();
     al_init_primitives_addon();
     al_install_keyboard();
 
-    janela = al_create_display(800, 600);
+    int x=800;
+    int y=600;
 
-    int x=0;
-    int y=0;
+    janela = al_create_display(x, y);
+    fEventos = al_create_event_queue();
 
-    al_clear_to_color(al_map_rgb(255,255,255));
+    al_register_event_source(fEventos, al_get_keyboard_event_source());
 
-    RETANGULO r = {600,80,15,15, 0,al_map_rgb(0,255,0)};
 
-    for(y = 0; y < 600; y += 20){
-        for(x = 0; x < 800; x+= 20){
-            desenhaRetangulo(r);
+    ALLEGRO_COLOR cor_grama = al_map_rgb(1,200,0);
+    ALLEGRO_COLOR cor_cobra = al_map_rgb(241, 94,255);
+    ALLEGRO_COLOR cor_maca = al_map_rgb(255,0,0);
 
-            r.x1 = x;
+    int xm = rand() % x;
+    int ym = rand() % y;
+
+    RETANGULO cCobra = {float(x/2),float(y/2),30,30,0,cor_cobra};
+    RETANGULO maca = {xm, ym, 30, 30, 0, cor_maca};
+
+    bool tecla[4] = {true,false,false,false}; // 0w/1a/2s/3d
+    bool gameover = false;
+    while(!gameover){
+
+        al_wait_for_event_timed(fEventos, &ev, 0.3);
+        al_clear_to_color(cor_grama);
+        desenhaRetangulo(maca);
+        desenhaRetangulo(cCobra);
+        al_flip_display();
+
+        if(ev.type == ALLEGRO_EVENT_KEY_DOWN){
+            if(ev.keyboard.keycode == ALLEGRO_KEY_W && !tecla[2]){
+                tecla[0] = true;
+                tecla[1] = false;
+                tecla[2] = false;
+                tecla[3] = false;
+            }
+            if(ev.keyboard.keycode == ALLEGRO_KEY_S && !tecla[0]){
+                tecla[0] = false;
+                tecla[1] = false;
+                tecla[2] = true;
+                tecla[3] = false;
+            }
+            if(ev.keyboard.keycode == ALLEGRO_KEY_A && !tecla[3]){
+                tecla[0] = false;
+                tecla[1] = true;
+                tecla[2] = false;
+                tecla[3] = false;
+            }
+            if(ev.keyboard.keycode == ALLEGRO_KEY_D && !tecla[1]){
+                tecla[0] = false;
+                tecla[1] = false;
+                tecla[2] = false;
+                tecla[3] = true;
+            }
+            if(ev.keyboard.keycode == ALLEGRO_KEY_C){
+                gameover = true;
+            }
         }
-        r.y1 = y;
+        if(tecla[0]){
+            cCobra.y1 -= 15;
+        }
+        if(tecla[1]){
+            cCobra.x1 -= 15;
+        }
+        if(tecla[2]){
+            cCobra.y1 += 15;
+        }
+        if(tecla[3]){
+            cCobra.x1 += 15;
+        }
+        if (cCobra.x1 < 0 || cCobra.x1 > x || cCobra.y1 < 0 || cCobra.y1> y){
+            gameover = true;
+        }
+
     }
-    al_flip_display();
-
-    getch();
-
-    al_clear_to_color(al_map_rgb(255,255,255));
-
     al_flip_display();
     al_rest(5);
 }
